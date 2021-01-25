6738249efd146e4b5d5d3a76c2153b912b4e9664
snoopytbe <49484910+snoopytbe@users.noreply.github.com>
2021-01-25 11:18:38 +0100

    l

--- src/components/Formulaire.js
+++ src/components/Formulaire.js
@@ -1,9 +1,8 @@
-import React from "react";
-import { useForm } from "react-hook-form";
-import { withStyles, makeStyles } from "@material-ui/core/styles";
-import TextField from "@material-ui/core/TextField";
+import React, { useState } from "react";
+import { useForm, Controller } from "react-hook-form";
 import Autocomplete from "@material-ui/lab/Autocomplete";
-import Grid from "@material-ui/core/Grid";
+import {TextField, Typography, Grid, IconButton} from '@material-ui/core';
+import {Icon} from '@material-ui/icons';
 import NumberFormat from "react-number-format";
 import * as constantes from "../data/constantes";
 
@@ -13,7 +12,13 @@ const numberFormat = (value) =>
     currency: "EUR"
   }).format(value);
 
-const TextFieldMontant = ({ name, register, disabled, onChangeHandler }) => {
+const TextFieldMontant = ({
+  name,
+  control,
+  register,
+  disabled,
+  onChangeHandler
+}) => {
   const props = (disabled) => {
     let conditionnalProperty = {};
     !disabled &&
@@ -111,21 +116,125 @@ function NumberFormatCustom(props) {
   );
 }
 export default function Formulaire() {
-  const { register, handleSubmit, watch, errors, setValue } = useForm();
+  const [data, setData] = useState([
+    { Montant: "1000", Categorie: "Habillement, accessoires" }
+  ]);
+  const {
+    register,
+    control,
+    handleSubmit,
+    watch,
+    errors,
+    setValue
+  } = useForm();
   const onSubmit = (data) => console.log(parseFloat(data.Montant_1));
+
+  const append = () => {
+    setData([...data, { Montant: "0", Categorie: "Dépenses vie courante" }]);
+  };
+
+  const remove = (index) => {
+    setData([...data.slice(0, index), ...data.slice(index + 1)]);
+  };
 
   return (
     <div style={{ flexGrow: 1 }}>
       <form onSubmit={handleSubmit(onSubmit)}>
-        <GridOperation numero={1} register={register} disabled={true} />
+        <GridOperation numero={98} register={register} disabled={true} />
         <GridOperation
-          numero={2}
+          numero={99}
           register={register}
           onChangeHandler={(e) => {
-            setValue("Montant_1", numberFormat(10 - e.target.value));
+            setValue("Montant_98", numberFormat(10 - e.target.value));
             //setValue("Montant_2", numberFormat(e.target.value));
           }}
         />
+        {data.map((item, index) => (
+          <Grid container direction="row" justify="center" spacing={2}>
+            <Grid item xs={3}>
+            <Typography>Dépense à découper</Typography>
+            </Grid>
+            <Grid item xs>
+              <Controller
+                control={control}
+                name={"Montant_" + index}
+                render={(
+                  { onChange, onBlur, value, name, ref },
+                  { invalid, isTouched, isDirty }
+                ) => (
+                  <TextField
+                    inputRef={register}
+                    label="Montant"
+                    variant="outlined"
+                    InputLabelProps={{
+                      shrink: true
+                    }}
+                    InputProps={{ inputComponent: NumberFormatCustom }}
+                    onChange={(e) => {
+                      setValue("Montant_0", numberFormat(10 - e.target.value));
+                    }}
+                    fullWidth
+                    defaultValue={item.Montant}
+                  />
+                )}
+              />
+            </Grid>
+            <Grid item xs>
+              <Controller
+                name={"Categorie_" + index}
+                control={control}
+                render={(
+                  { onChange, onBlur, value, name, ref },
+                  { invalid, isTouched, isDirty }
+                ) => (
+                  <Autocomplete
+                    id="autocomplete"
+                    options={constantes.data}
+                    getOptionLabel={(option) => option.label}
+                    fullWidth
+                    required
+                    renderInput={(params) => (
+                      <TextField
+                        {...params}
+                        variant="outlined"
+                        inputRef={register({ required: true })}
+                        required
+                        defaultValue={item.Categorie}
+                        label="Catégorie"
+                      />
+                    )}
+                  />
+                )}
+              />
+            </Grid>
+            <Grid item xs={1}>
+              <span
+                role="img"
+                aria-label="action"
+                onClick={() =>
+                  index < data.length - 1 ? remove(index) : append()
+                }
+                style={{ cursor: "pointer" }}
+              >
+                {index < data.length - 1 ? "✖" : "➕"}
+              </span>
+            </Grid>
+            <IconButton aria-label="delete">
+        <DeleteIcon />
+      </IconButton>
+            <Grid item xs={1}>
+              <span
+                role="img"
+                aria-label="action"
+                onClick={() => index === data.length - 1 && remove(index)}
+                style={{ cursor: "pointer" }}
+              >
+                {index === data.length - 1 && "✖"}
+              </span>
+            </Grid>
+          </Grid>
+        ))}
+
         <input type="submit" />
       </form>
     </div>
